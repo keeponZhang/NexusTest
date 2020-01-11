30df55c950a5deeaafb6dab453f97d40b93a4dee
zhangwengao <zhangwengao@yy.com>
2020-01-11 16:00:45 +0800

    no message

---
+++ nesuslibrary/src/main/java/com/zhang/nesuslibrary/NewNexusUtils.java
@@ -0,0 +1,17 @@
+package com.zhang.nesuslibrary;
+
+/**
+ * createBy	 keepon
+ */
+public class NewNexusUtils {
+    public   static String   sayHello(){
+        return "this is from NexusLibrary sayHello";
+    }
+    public   static String   sayHello2(){
+        return "this is from NexusLibrary sayHello222222222";
+    }
+}
+
+//1计算机管理启动服务
+//2E:\sofeware\nexus\nexus-3.17.0-01-win64\nexus-3.17.0-01\etc\nexus-default.properties 修改端口号为8181
+//3.repo.yypm.com 映射成本机地址

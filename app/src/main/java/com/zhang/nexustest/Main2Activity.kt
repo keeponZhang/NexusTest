0d10d5b05c79442f45cdf426c391566c18434564
zhangwengao <zhangwengao@yy.com>
2020-08-11 16:47:53 +0800

    no message

--- app/src/main/java/com/zhang/nexustest/Main2Activity.kt
+++ app/src/main/java/com/zhang/nexustest/Main2Activity.kt
@@ -1,7 +1,7 @@
 package com.zhang.nexustest
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class Main2Activity : AppCompatActivity() {
 
